e2202a46017a55d7e86ff4ddd0145df52c7fb916
Anand Sinha <anandsinha138@gmail.com>
2026-08-28 05:06:19 +0530

    Progress Aug 28

---
+++ leetcode/169.majority-element.cs
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=169 lang=csharp
+ *
+ * [169] Majority Element
+ *
+ * https://leetcode.com/problems/majority-element/description/
+ *
+ * algorithms
+ * Easy (66.51%)
+ * Likes:    23184
+ * Dislikes: 837
+ * Total Accepted:    5.9M
+ * Total Submissions: 8.9M
+ * Testcase Example:  '[3,2,3]'
+ *
+ * Given an array nums of size n, return the majority element.
+ * 
+ * The majority element is the element that appears more than ⌊n / 2⌋ times.
+ * You may assume that the majority element always exists in the array.
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [3,2,3]
+ * Output: 3
+ * Example 2:
+ * Input: nums = [2,2,1,1,1,2,2]
+ * Output: 2
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * n == nums.length
+ * 1 <= n <= 5 * 10^4
+ * -10^9 <= nums[i] <= 10^9
+ * The input is generated such that a majority element will exist in the
+ * array.
+ * 
+ * 
+ * 
+ * Follow-up: Could you solve the problem in linear time and in O(1) space?
+ */
+
+// @lc code=start
+public class Solution {
+    public int MajorityElement(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
